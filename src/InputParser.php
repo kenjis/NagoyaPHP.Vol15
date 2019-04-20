03c9188f5a219827b4d231647f19548568708319
Kenji Suzuki <kenji.uui@gmail.com>
2019-04-20 11:33:36 +0900

    refactor: change numbers to variable

--- src/InputParser.php
+++ src/InputParser.php
@@ -12,44 +12,60 @@ class InputParser
 
         $input = (int) $input;
 
-        if ($input >= 128) {
+        $n = 128;
+
+        if ($input >= $n) {
             $output[] = 7;
-            $input = $input - 128;
+            $input = $input - $n;
         }
 
-        if ($input >= 64) {
+        $n = $n / 2;
+
+        if ($input >= $n) {
             $output[] = 6;
-            $input = $input - 64;
+            $input = $input - $n;
         }
 
-        if ($input >= 32) {
+        $n = $n / 2;
+
+        if ($input >= $n) {
             $output[] = 5;
-            $input = $input - 32;
+            $input = $input - $n;
         }
 
-        if ($input >= 16) {
+        $n = $n / 2;
+
+        if ($input >= $n) {
             $output[] = 4;
-            $input = $input - 16;
+            $input = $input - $n;
         }
 
-        if ($input >= 8) {
+        $n = $n / 2;
+
+        if ($input >= $n) {
             $output[] = 3;
-            $input = $input - 8;
+            $input = $input - $n;
         }
 
-        if ($input >= 4) {
+        $n = $n / 2;
+
+        if ($input >= $n) {
             $output[] = 2;
-            $input = $input - 4;
+            $input = $input - $n;
         }
 
-        if ($input >= 2) {
+        $n = $n / 2;
+
+        if ($input >= $n) {
             $output[] = 1;
-            $input = $input - 2;
+            $input = $input - $n;
         }
 
-        if ($input === 1) {
+        $n = $n / 2;
+
+        if ($input === $n) {
             $output[] = 0;
-            $input = $input - 1;
+            $input = $input - $n;
         }
 
         sort($output);
